1c04fc6f801831116ba893baf79ebe24601a75d1
bonbo36 <shino.yamane99@gmail.com>
2023-03-31 19:15:39 +0900

    商品出品機能②

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -53,25 +53,25 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Estimated delivery date can't be blank")
       end
-      it '販売価格が---だと保存できない' do
-        @item.price = 1
+      it '販売価格が空だと保存できない' do
+        @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it '販売価格が¥300未満のとき保存できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '販売価格が¥9,999,999より大きいと保存できない' do
         @item.price = 1_000_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it '販売価格が半角数字以外のとき登録できない' do
         @item.price = 'aaaaaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'ユーザーが紐付いていなければ出品できない' do
         @item.user = nil
